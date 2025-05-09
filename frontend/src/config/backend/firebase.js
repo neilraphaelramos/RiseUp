1b517fff13bf360e39c1af4889f17445475c7db1
neilraphaelramos <neilraphael.ramos4@gmail.com>
2025-05-09 16:33:50 +0800

    Fixing the firebase.js

--- frontend/src/config/backend/firebase.js
+++ frontend/src/config/backend/firebase.js
@@ -3,14 +3,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuHG6HPca8q4qE0Hp_0cgWIgJZXAilcdo",
+  apiKey: import.meta.env.VITE_APIKEY,
   authDomain: "oneup-1123b8b.firebaseapp.com",
   databaseURL: "https://oneup-1123b8b-default-rtdb.firebaseio.com",
   projectId: "oneup-1123b8b",
   storageBucket: "oneup-1123b8b.appspot.com",
   messagingSenderId: "767606765314",
-  appId: "1:767606765314:web:ef57203d72697dfd4ff41b",
-  measurementId: "G-90RYJX75H4"
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
